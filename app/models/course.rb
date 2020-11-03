9c32bb4acef4885d9a5011a6254a281922fcb0c6
issarakoat <weeravut.issarakoat@gmail.com>
2020-11-03 06:18:04 +0000

    deep filter courses

--- app/models/course.rb
+++ app/models/course.rb
@@ -1,13 +1,26 @@
+  
 class Course < ApplicationRecord
-  validates :title, :short_description, :language, :price, :level, presence: true
+  validates :title, :short_description, :language, :price, :level,  presence: true
   validates :description, presence: true, length: { :minimum => 5 }
   
-  belongs_to:user
+  belongs_to :user
+
   def to_s
     title
   end
   has_rich_text :description
-  
+
   extend FriendlyId
   friendly_id :title, use: :slugged
+
+  LANGUAGES = [:"English", :"Russian", :"Polish", :"Spanish"]
+  def self.languages
+    LANGUAGES.map { |language| [language, language] }
+  end
+
+  LEVELS = [:"Beginner", :"Intermediate", :"Advanced"]
+  def self.levels
+    LEVELS.map { |level| [level, level] }
+  end
+
 end
