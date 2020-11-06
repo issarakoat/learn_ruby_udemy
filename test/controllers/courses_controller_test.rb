08d80e135d7cef2852d70b2e9ac2a1b0145d09d9
issarakoat <weeravut.issarakoat@gmail.com>
2020-11-06 19:22:53 +0000

    finish search stuff with ransack gem

--- test/controllers/courses_controller_test.rb
+++ test/controllers/courses_controller_test.rb
@@ -1,48 +1,70 @@
-require 'test_helper'
+class CoursesController < ApplicationController
+  before_action :set_course, only: [:show, :edit, :update, :destroy]
 
-class CoursesControllerTest < ActionDispatch::IntegrationTest
-  setup do
-    @course = courses(:one)
+  def index
+    #if params[:title]
+    #  @courses = Course.where('title ILIKE ?', "%#{params[:title]}%") #case-insensitive
+    #else
+    #  #@courses = Course.all
+    #  
+    #  #@q = Course.ransack(params[:q])
+    #  #@courses = @q.result.includes(:user)
+    #end
+    @ransack_courses = Course.ransack(params[:courses_search], search_key: :courses_search)
+    @courses = @ransack_courses.result.includes(:user)
   end
 
-  test "should get index" do
-    get courses_url
-    assert_response :success
+  def show
   end
 
-  test "should get new" do
-    get new_course_url
-    assert_response :success
+  def new
+    @course = Course.new
   end
 
-  test "should create course" do
-    assert_difference('Course.count') do
-      post courses_url, params: { course: { description: @course.description, title: @course.title } }
+  def edit
+  end
+
+  def create
+    @course = Course.new(course_params)
+    @course.user = current_user
+
+    respond_to do |format|
+      if @course.save
+        format.html { redirect_to @course, notice: 'Course was successfully created.' }
+        format.json { render :show, status: :created, location: @course }
+      else
+        format.html { render :new }
+        format.json { render json: @course.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  def update
+    respond_to do |format|
+      if @course.update(course_params)
+        format.html { redirect_to @course, notice: 'Course was successfully updated.' }
+        format.json { render :show, status: :ok, location: @course }
+      else
+        format.html { render :edit }
+        format.json { render json: @course.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  def destroy
+    @course.destroy
+    respond_to do |format|
+      format.html { redirect_to courses_url, notice: 'Course was successfully destroyed.' }
+      format.json { head :no_content }
+    end
+  end
+
+  private
+    def set_course
+      @course = Course.friendly.find(params[:id])
     end
 
-    assert_redirected_to course_url(Course.last)
-  end
-
-  test "should show course" do
-    get course_url(@course)
-    assert_response :success
-  end
-
-  test "should get edit" do
-    get edit_course_url(@course)
-    assert_response :success
-  end
-
-  test "should update course" do
-    patch course_url(@course), params: { course: { description: @course.description, title: @course.title } }
-    assert_redirected_to course_url(@course)
-  end
-
-  test "should destroy course" do
-    assert_difference('Course.count', -1) do
-      delete course_url(@course)
+    def course_params
+      params.require(:course).permit(:title, :description, :short_description, :price, :language, :level)
     end
-
-    assert_redirected_to courses_url
-  end
 end
