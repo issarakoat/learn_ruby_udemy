b6f1849c991e4be28ceac605d71cad1f09577144
issarakoat <weeravut.issarakoat@gmail.com>
2020-11-08 17:07:33 +0000

    added roles

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,10 @@
 class User < ApplicationRecord
+  
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable, :trackable, :confirmable
-         
+  rolify       
   def to_s
     email
   end
@@ -13,4 +14,17 @@ class User < ApplicationRecord
   end
   
   has_many :courses
+  
+  after_create :assign_default_role
+
+  def assign_default_role
+    if User.count == 1
+      add_role(:admin) if roles.blank?
+      add_role(:teacher)
+      add_role(:student)
+    else
+      add_role(:student) if roles.blank?
+      add_role(:teacher) # if you want any user to be able to create own courses
+    end
+  end
 end
--- db/migrate/20201102034533_add_fields_to_courses.rb
+++ db/migrate/20201102034533_add_fields_to_courses.rb
@@ -1,4 +1,4 @@
-class AddFieldsToCourses < ActiveRecord::Migration[6.0]
+class AddFieldsToCourses < ActiveRecord::Migration
   def change
     add_column :courses, :short_description, :text
     add_column :courses, :language, :string, default: "English", null: false
